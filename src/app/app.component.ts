1d85e72622c2b86a900375ab00522a42b3710b4f
Grabarz <grzegorz.grabarz@capgemini.com>
2017-02-16 17:36:39 +0100

    cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  title = 'Zombies!';
+  skills: Array<any>;
+
+  constructor(private http: Http) { }
+
+  ngOnInit() {
+    this.http.get('/assets/skills.json').subscribe(
+      res => {
+        this.skills = res.json().data;
+      });
+  }
+
+
 }
