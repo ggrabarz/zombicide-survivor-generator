3e30dabf6c6abcf9f27a9c2d6e01a55773deed2b
Grzegorz Grabarz <grzegorz.grabarz+git@gmail.com>
2017-02-18 18:25:55 +0100

    skills and survivors filtering

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, trigger, state, style, transition, animate } from '@angular/core';
 import { Http } from '@angular/http';
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/debounceTime';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('survivorState', [
+      transition('* => void', [
+        style({ opacity: '*' }),
+        animate("0.5s ease-in", style({ opacity: 0 }))
+      ]),
+      transition('void => false', [
+        /*no transition on first load*/
+      ]),
+      transition('void => *', [
+        style({ opacity: 0 }),
+        animate("0.5s ease-in", style({ opacity: '*' }))
+      ])
+    ])
+  ]
 })
 export class AppComponent implements OnInit {
   skills: Array<any>;
+  skillsRaw = Array<any>();
   survivors: Array<any>;
-  sets: any = {};
+  survivorsRaw = Array<any>();
+
+  survivorFilter$ = new Subject<string>();
+  survivorFilter = '';
+
+  skillFilter$ = new Subject<string>();
+  skillFilter = '';
+
   set = {
     bs: true,
     wb: true,
@@ -20,18 +45,59 @@ export class AppComponent implements OnInit {
   constructor(private http: Http) { }
 
   ngOnInit() {
-    this.http.get('/assets/static-data/skills.1.json').subscribe(
+    this.http.get('/assets/static-data/skills.json').subscribe(
       res => {
-        this.skills = res.json().data;
+        this.skillsRaw = res.json().data;
+        this.skills = this.skillsRaw;
       });
 
     this.http.get('/assets/static-data/survivors.json').subscribe(
       res => {
-        this.survivors = res.json().data.filter(s => s.name !== '');
+        this.survivorsRaw = res.json().data.filter(s => s.name !== '');
+        this.survivors = this.survivorsRaw;
       });
+
+    this.survivorFilter$.debounceTime(100).subscribe(name => {
+      this.survivorFilter = name.toLowerCase();
+      this.filterSurvivors();
+    });
+
+    this.skillFilter$.debounceTime(100).subscribe(skill => {
+      this.skillFilter = skill.toLowerCase();
+      this.filterSkills();
+    });
   }
 
-  toggle(flag: boolean) {
-    flag = !flag;
+  filterSkills() {
+    this.skills = this.skillsRaw.filter(skill => skill.name.toLowerCase().includes(this.skillFilter) || skill.desc.toLowerCase().includes(this.skillFilter));
+  }
+
+  filterSurvivors() {
+    this.survivors = this.filterSurvivorsBySet()
+      .filter(sur => sur.name.toLowerCase().includes(this.survivorFilter))
+  }
+
+  private filterSurvivorsBySet(): Array<any> {
+    let survivorsFiltered = this.survivorsRaw.filter(sur => this.filterSet(sur))
+    return survivorsFiltered;
+  }
+
+  private filterSet(survivor: any): boolean {
+    let set;
+    switch (survivor.set) {
+      case 'Black Plague Base':
+        set = this.set.bs;
+        break;
+      case 'Wulfsburg':
+        set = this.set.wb;
+        break;
+      case 'Hero Box':
+        set = this.set.hb;
+        break;
+      case 'Guest Box':
+        set = this.set.gb;
+        break;
+    }
+    return set;
   }
 }
