d789148af8a15132b3e226a6bc9feb7eedaa2e0c
Grabarz <grzegorz.grabarz@capgemini.com>
2017-03-01 15:34:49 +0100

    css -> scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { Skill, Survivor } from './model/index';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
 
